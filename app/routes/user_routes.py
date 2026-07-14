c48e2ac6d76c1bde482e403d4b92cc3849aea4ac
Priyanshu <sonipriyanshu029@gmail.com>
2026-07-15 05:00:44 +0530

    a small import change

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, request, jsonify, send_from_directory
 from ..models.user import User
 from ..services import face_service
 from config import Config
+from extensions import db
 
 user_bp = Blueprint('user', __name__)
 
